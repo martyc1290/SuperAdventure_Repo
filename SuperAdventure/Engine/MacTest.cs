7f7d0f2485645c4a68088623d15e02ba854b0dc2
martymac1290 <martyc1290@gmail.com>
2018-05-22 00:05:07 -0500

    Testing Mac (again)
    Added MacTest.cs class to Engine. Testing ability to use MacBook for project.

---
+++ SuperAdventure/Engine/MacTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Engine
+{
+    public class MacTest
+    {
+        public MacTest()
+        {
+			// This is testing whether or not I can work on this file on my Mac
+        }
+    }
+}
