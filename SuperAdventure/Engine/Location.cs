bc0cc771a523ab7e33463d23b0e0a748a844aa25
martymac1290 <martyc1290@gmail.com>
2018-05-22 09:45:32 -0500

    9 Using class constructors with derived classes
    Created class properties that can use other classes as its datatype. Edited Location and Quest classes.

--- SuperAdventure/Engine/Location.cs
+++ SuperAdventure/Engine/Location.cs
@@ -14,14 +14,26 @@ namespace Engine
         public string Name { get; set; }
         public string Description { get; set; }
 
+		public Item ItemRequiredToEnter { get; set; }
+		public Quest QuestAvailableHere { get; set; }
+		public Monster MonsterLivingHere { get; set; }
+		public Location LocationToNorth { get; set; }
+		public Location LocationToEast { get; set; }
+		public Location LocationToSouth { get; set; }
+		public Location LocationToWest { get; set; }
+
         // This is a constructor
         // It is public to see it anywhere in the solution, has a name of the class, and 3 parameters
-        public Location(int id, string name, string description)
+        public Location(int id, string name, string description, Item itemRequiredToEnter = null,
+		                Quest questAvailableHere = null, Monster monsterLivingHere = null)
 		{
             // Properties/Variable = Parameters
 			ID = id;
 			Name = name;
 			Description = description;
+			ItemRequiredToEnter = itemRequiredToEnter;
+			QuestAvailableHere = questAvailableHere;
+			MonsterLivingHere = monsterLivingHere;
 		}
     }
 }
