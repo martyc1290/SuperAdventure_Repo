c9ee2f7e8d7e97291be812f4ec983bd50cc8e498
Marty Cawley <martyc1290@yahoo.com>
2018-05-23 14:35:31 -0500

    10 Creating collections of objects
    Added InventoryItem, PlayerQuest, QuestCompletionItem, and LootItem classes, and added empty list properties to Player, Monster, and Quest classes to store player items, quests, etc.

--- SuperAdventure/Engine/Player.cs
+++ SuperAdventure/Engine/Player.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; // This allows program will find everything it needs to work with collections
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +16,20 @@ namespace Engine
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
 
+        // These are two properties that can hold lists containing InventoryItem and PlayerQuests objects
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
+
         // This is the constructor of the Player class
 		public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints,
 		              int level) : base(currentHitPoints, maximumHitPoints)
 		{
 			Gold = gold;
 			ExperiencePoints = experiencePoints;
-			Level = level;           
+			Level = level;
+            // The following two lines set the value of the new properties to empty lists, allowing items to be added to them later
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
 		}
     }
 }
