a92b7af15b0092097b92bd6288dc89e87f51fbda
Marty Cawley <martyc1290@yahoo.com>
2018-07-22 13:56:40 -0500

    16a Writing the function to move the player
    Began writing the code required to move the player. Added MoveTo method to SuperAdventure.cs and added MoveTo to button click methods.

--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -18,18 +18,18 @@ namespace SuperAdventure
         // Creating a class level variable by having it inside class, but outside functions or methods
         // Scope Datatype Name
         private Player _player;
+        private Monster _currentMonster;
 
         public SuperAdventure()
         {
             // This is the constructor for the class, gets called when we create a new ofject of the class - in this case, the form
             InitializeComponent();
 
-            // Now using the default constructor of Location
-			Location location = new Location(1, "Home", "This is your house.");
-            
             // Creates a new Player object, and assigns it to the object _player
             // Assign values to the properties of the _player object using the Constructor created in the Player class
 			_player = new Player(10, 10, 20, 0,1);
+            MoveTo(World.LocationByID.(World.LOCATION_ID_HOME));
+            _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
 
             // Get the values of the properties of the _player object and assign them to the labels on screen
             lblHitPoints.Text = _player.CurrentHitPoints.ToString();
@@ -38,31 +38,56 @@ namespace SuperAdventure
             lblLevel.Text = _player.Level.ToString();
         }
 
-
-
+        // Not sure where this load function came from? Maybe delete if not needed...
         private void SuperAdventure_Load(object sender, EventArgs e)
         {
-
         }
 
         private void btnNorth_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToNorth);
         }
 
         private void btnSouth_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToSouth);
         }
 
         private void btnEast_Click(object sender, EventArgs e)
         {
-
+            MoveTo(_player.CurrentLocation.LocationToEast);
         }
 
         private void btnWest_Click(object sender, EventArgs e)
         {
+            MoveTo(_player.CurrentLocation.LocationToSouth);
+        }
 
+        private void MoveTo(Locaiton newLocation)
+        {
+            // Does the location have any required items
+            if (newLocation.ItemRequiredToEnter != null)
+            {
+                // See if the player has the required item in their inventory
+                bool playerHasRequiredItem = false;
+
+                foreach (InventoryItem ii in _player.Inventory)
+                {
+                    if (ii.Details.ID == newLocation.ItemRequiredToEnter.ID)
+                    {
+                        // We found the required item
+                        playerHasRequiredItem = true;
+                        break; // Exit out of the foreach loop
+                    }
+                }
+
+                if (!playerHasRequiredItem)
+                {
+                    // We didn't find the required item in their inventory, so display a message and stop trying to move
+                    rtbMessages.Text += "You must have a " + newLocation.ItemRequiredToEnter.Name + " to enter this location." + Environment.NewLine;
+                    return;
+                }
+            }
         }
 
         private void btnUseWeapon_Click(object sender, EventArgs e)
